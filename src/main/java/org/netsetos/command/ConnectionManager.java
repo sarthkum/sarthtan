1a32428f49f26fe819afcc17a2750e79c34935ea
sarthak0488 <sarthak.varshneyt@outlook.com>
2017-09-25 21:28:17 +0530

    test

---
+++ src/main/java/org/netsetos/command/ConnectionManager.java
@@ -0,0 +1,82 @@
+package org.netsetos.command;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.http.*; 
+import javax.sql.*;
+import javax.naming.*;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import com.google.api.client.json.gson.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+//import com.google.api.client.json.JsonFactory;
+////import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.services.translate.Translate;
+//import com.google.api.services.translate.Translate.Translations;
+//import com.google.api.services.translate.model.TranslationsListResponse;
+//import com.google.api.services.translate.model.TranslationsResource;
+
+public class ConnectionManager extends HttpServlet  {
+	
+	private static final long serialVersionUID = 1L;
+	 
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Connection result = null;
+		HttpSession session=request.getSession();
+		ResultSet rs=null;
+		ResultSet rsemail=null;  
+		Statement stmt = null;
+		Statement stmte = null; 
+		try {
+			// Grab the variables from the form.
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			String email = request.getParameter("email") ;
+			String emailpassword  = "fromemail";
+			System.out.println("email is "+email);
+			
+			String postuser = System.getenv("PG_USERNAME");
+			
+			String postpass = System.getenv("DATABASE_PASSWORD");
+			
+			String postdata = System.getenv("DATABASE_PASSWORD");
+			
+			System.out.println("postuser"+postuser);
+			
+			System.out.println("postpass"+postpass);
+			
+			System.out.println("postdata"+postdata);
+
+		    Context initialContext = new InitialContext();
+		    DataSource datasource = (DataSource)initialContext.lookup("java:jboss/datasources/PostgreSQLDS");
+		    result = datasource.getConnection();
+			//response.sendRedirect("EnvSetup.jsp");
+			    
+		} catch (Exception ex) {
+			response.sendRedirect("EnvSetup.jsp");
+			ex.printStackTrace();
+		    System.out.println("Exception:  " + ex + ex.getMessage());
+		}
+		finally{
+		    try { if (rs != null) rs.close(); } catch (Exception e) {};
+		    try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+		    try { if (result != null) result.close(); } catch (Exception e) {};
+	}
+
+		
+	}
+
+}
